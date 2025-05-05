87452e65a76d8e3a5dccd56e9edb1c0c4752e657
Nishi Surti <nishisurti01@gmail.com>
2025-05-05 13:17:10 -0400

    Fetch meme data using useEffect()

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
 
 
 
 export default function Main() {
 
    const [meme,setMeme] = useState({
-
-
     topText : "One does not simply",
     bottomText : "Walk into Mordor" ,
     imageURL : "http://i.imgflip.com/1bij.jpg"
 })
+    const [allMemes, setAllMemes] = useState([])
 
+    
+
+useEffect(()=> {
+fetch("https://api.imgflip.com/get_memes")
+.then(res => res.json())
+.then (data => setAllMemes(data.data.memes))
+},[])
 
 function handleChange(event){
 const {value,name} = event.currentTarget
